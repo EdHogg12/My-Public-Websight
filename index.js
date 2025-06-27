cc21e15efdb289024c314cce8bb3102f4d68daa1
EdHogg12 <hoggedmund72@gmail.com>
2025-06-27 18:21:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+ const display = document.getElementById("display");
+
+
+ display.value = "Edmund Hogg";
+
+function Music(){
+  window.open('https://edhogg12.github.io/My-Public-Websight/Music', '_self');
+}
