df07b56582853401cd51f9f1ab8666d1e19c31f1
EdHogg12 <hoggedmund72@gmail.com>
2025-06-27 17:48:04 +0100

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,24 @@
+ const display = document.getElementById("display");
+
+
+ display.value = "Edmund Hogg";
+
+function TikTok(){
+  window.open('https://www.tiktok.com/@machinehum', '_self');
+}
+
+function Instagram(){
+  window.open('file:///C:/Users/Edmund/OneDrive/Desktop/coding/My%20Websight/Home/Projects.html', '_self');
+}
+
+function YouTube(){
+  window.open('https://www.youtube.com/@MachineHum', '_self');
+}
+
+function Goto11(){
+  window.open('https://www.youtube.com', '_blank');
+}
+
+function Goto12(){
+  window.open('https://www.chess.com/play', '_blank');
+}
