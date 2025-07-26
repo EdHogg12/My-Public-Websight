0668c5c45ce18353e18387622568de1cd7500285
EdHogg12 <hoggedmund72@gmail.com>
2025-07-26 19:12:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
- const display = document.getElementById("display");
 
+function GotoTikTok() {
+    window.location.href = "https://www.tiktok.com/@machinehum";
+}
+function GotoYoutube() {
+    window.location.href = "https://www.youtube.com/@MachineHum";
+}
+function GotoInstagram() {
+    window.location.href = "https://www.instagram.com/machinehum_";
+}
+function GotoSpotify() {
+    window.location.href = "https://www.spotify.com/";
+}
+function GotoApplemusic() {
+    window.location.href = "https://www.applemusic.com/";
+}
+function Gotosoundcloud() {
+    window.location.href = "https://www.soundcloud.com/";
+}
+function GotoMore() {
+    window.location.href = "more.html";
+}
 
- display.value = "Edmund Hogg";
+document.addEventListener('DOMContentLoaded', function() {
+    const text = document.querySelector('.animated-text');
+    if (text) {
+        text.classList.add('loaded');
+    }
+});
 
-function Music(){
-  window.open('https://edhogg12.github.io/My-Public-Websight/Music', '_self');
+function toggleDropdown() {
+    document.getElementById("moreDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
